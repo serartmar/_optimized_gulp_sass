4b24f66c99833d705c66e91af5a69bf5e6de68b0
Alex <omelchenkoalexsandr@gmail.com>
2016-11-28 13:19:44 +0200

    test

---
+++ app/js/common.js
@@ -0,0 +1,52 @@
+$(function() {
+
+	$(".top-line .sf-menu").superfish({
+		cssArrows: false,
+		hoverClass:'no-class',
+			delay: 200
+		});
+
+		$(".slider").owlCarousel({
+			loop: true,
+			items:1,
+			itemClass: "slide-wrap",
+		});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	}
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	}
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+});
